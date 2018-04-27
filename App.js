5923f4d01bc6c433146dcbfd2737ba8ed833f9ba
xiaofeng <724332274@qq.com>
2018-04-27 17:22:31 +0800

    change

--- App.js
+++ App.js
@@ -132,12 +132,18 @@ class App extends PureComponent {
         CodePush.sync(
             {
                 updateDialog: {
-                    optionalIgnoreButtonLabel: '稍后',
+                    optionalIgnoreButtonLabel: '下次再说',
                     optionalInstallButtonLabel: '后台更新',
                     optionalUpdateMessage: '有新版本了，是否更新？',
                     title: '更新提示'
                 },
-                installMode: CodePush.InstallMode.IMMEDIATE,
+                /**
+                 * 三种更新的策略: 配置到installMode: 之后即可生效
+                 * IMMEDIATE 下载完立即更新APP
+                 * ON_NEXT_RESTART 到下一次启动应用时
+                 * ON_NEXT_RESUME 当应用从后台返回时
+                 */
+                installMode: CodePush.InstallMode.ON_NEXT_RESTART,
             },
             this._codePushStatusDidChange,
             this._codePushDownloadDidProgress
